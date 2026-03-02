91dce9a5b8ed262ec16d3bcb81f1e452e1872343
Azealea <auhubert6@gmail.com>
2026-03-02 20:38:57 +0100

    fix: non blurry textures

--- vulkan_hello_world/src/vk/vk_texture.c
+++ vulkan_hello_world/src/vk/vk_texture.c
@@ -298,18 +298,20 @@ void create_texture_sampler(App* app)
 
     VkSamplerCreateInfo samplerInfo = {
         .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
-        .magFilter = VK_FILTER_LINEAR,
-        .minFilter = VK_FILTER_LINEAR,
+        .magFilter = VK_FILTER_NEAREST,
+        .minFilter = VK_FILTER_NEAREST,
         .addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT,
         .addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT,
         .addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT,
-        .anisotropyEnable = VK_TRUE,
-        .maxAnisotropy = properties.limits.maxSamplerAnisotropy,
+        .anisotropyEnable = VK_FALSE,
+        .maxAnisotropy = 1.0f,
         .borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK,
         .unnormalizedCoordinates = VK_FALSE,
         .compareEnable = VK_FALSE,
         .compareOp = VK_COMPARE_OP_ALWAYS,
-        .mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR,
+        .mipmapMode = VK_SAMPLER_MIPMAP_MODE_NEAREST,
+        .minLod = 0.0f,
+        .maxLod = 0.0f,
     };
 
     ASSERTVK(vkCreateSampler(app->context.device, &samplerInfo, app->allocator,
