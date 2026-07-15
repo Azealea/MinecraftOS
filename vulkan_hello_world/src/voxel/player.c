2fdb4821c9966bad209abd255b8165f71547b004
Azealea <auhubert6@gmail.com>
2026-07-15 02:18:39 +0200

    feat(player): hook place&break blocks to re mesh

--- vulkan_hello_world/src/voxel/player.c
+++ vulkan_hello_world/src/voxel/player.c
@@ -4,35 +4,34 @@
 #include "raycast.h"
 #include "utils/log.h"
 #include "utils/vec.h"
+#include "vk/vk.h"
 #include "voxel/block.h"
 #include "voxel/chunk.h"
 #include "voxel/world.h"
 
-static void trigger_mesh_rebuild(World* w, Chunk* c)
-{}
-
-static void break_on_hit(World* w, RaycastHit hit)
+static void break_on_hit(App* app, RaycastHit hit)
 {
-    Chunk* c = world_get_or_add_chunk(w, pos_glob_to_chunk(hit.block));
+    ChunkPos cpos = pos_glob_to_chunk(hit.block);
+    Chunk* c = world_get_chunk(&app->world, cpos);
 
     VEC3(u8) local = pos_glob_to_rel(hit.block);
     chunk_set(c, local, ((Block){.type = BLK_AIR}));
 
     TRACE("breaking " VEC3_FMT, VEC3_ARGS(local));
-    trigger_mesh_rebuild(w, c);
+    vk_rebuild_mesh(app, cpos);
 }
 
-static void place_on_hit(World* w, RaycastHit hit, Block block)
+static void place_on_hit(App* app, RaycastHit hit, Block block)
 {
     VEC3(i32) blockPlace = VEC3_ADD(hit.block, hit.normal);
-
-    Chunk* c = world_get_or_add_chunk(w, pos_glob_to_chunk(blockPlace));
+    ChunkPos cpos = pos_glob_to_chunk(blockPlace);
+    Chunk* c = world_get_or_add_chunk(&app->world, cpos);
 
     VEC3(u8) local = pos_glob_to_rel(blockPlace);
     chunk_set(c, local, block);
 
     TRACE("placing " VEC3_FMT, VEC3_ARGS(local));
-    trigger_mesh_rebuild(w, c);
+    vk_rebuild_mesh(app, cpos);
 }
 
 void player_consume_input(App* app)
@@ -44,12 +43,12 @@ void player_consume_input(App* app)
     {
         RaycastHit hit = raycast(&app->camera, &app->world, 8.0f);
         if (hit.hit)
-            break_on_hit(&app->world, hit);
+            break_on_hit(app, hit);
     }
     if (input_mouse_pressed(&app->inputState, 1))
     {
         RaycastHit hit = raycast(&app->camera, &app->world, 8.0f);
         if (hit.hit)
-            place_on_hit(&app->world, hit, (Block){.type = BLK_DIRT});
+            place_on_hit(app, hit, (Block){.type = BLK_DIRT});
     }
 }
